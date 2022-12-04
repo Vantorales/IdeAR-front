c206eae68f8028f1e88b8f7352c1e0b1f2a32527
Vanesa <vanetorales98@gmail.com>
2022-12-04 14:58:01 -0300

    se creó funcionalidad de editar publicacion

--- js/editarPublicacion.js
+++ js/editarPublicacion.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     const titulo = document.querySelector("#tituloPublicacion");
     const contenido = document.querySelector("#contenido");
     const fechaCreacion =  document.querySelector("#fechaCreacion");
+    const editTitulo = document.querySelector("#editTitulo");
+    const editContenido = document.querySelector("#editContenido");
 
     console.log(dataPublicacion);
 
@@ -23,6 +25,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     contenido.innerText = dataPublicacion.publicacion[0].contenido ? dataPublicacion.publicacion[0].contenido: "";
     fechaCreacion.innerText = dataPublicacion.publicacion[0].fechaCreacion ? dataPublicacion.publicacion[0].fechaCreacion: "";
     nickname.innerText = dataPublicacion.publicacion[0].usuario.nickname ? dataPublicacion.publicacion[0].usuario.nickname: "";
+    editTitulo.value = dataPublicacion.publicacion[0].titulo ? dataPublicacion.publicacion[0].titulo: "";
+    editContenido.value = dataPublicacion.publicacion[0].contenido ? dataPublicacion.publicacion[0].contenido: "";
 
 
 });
@@ -32,19 +36,18 @@ const editarPublicacion = () => {
 
     let datosPublicacion; 
 
-    const containerTitulo = document.querySelector("#containerTitulo");
-    const contenido = document.querySelector("#contenido").value;
-    const fechaCreacion =  document.querySelector("#fechaCreacion").value;
+    const titulo = document.querySelector("#editTitulo").value;
+    const contenido = document.querySelector("#editContenido").value;
 
     const data = {
         titulo,
         contenido,
-        fechaCreacion,
-        usupublicacion
-    }
+        id: "638bd063534c1a6e925db3a8"
+        }
 
     const url = "http://localhost:8080/api/editarPublicacion";
     sendData(url, data, "PUT");
+    location.reload();
 
 }
 
